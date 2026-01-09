156f17e950ba84fd2f66c313bbb535e78df8469e
Muhammad Moiz Naveed <mznxnvv@gmail.com>
2026-01-10 01:12:53 +0400

    Initial commit of StudentVerse website

---
+++ client/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+// Global styles from the original site
+import './styles/page.css'
+import './styles/about.css'
+import './styles/contact.css'
+import './styles/react-fixes.css'
+import './styles/overrides.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
